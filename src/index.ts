28f3172b68dac4598371a661ea12626bbc3ad9c7
miyamae <miya.y.1201@gmail.com>
2019-12-22 22:45:09 +0900

    add: git-remote-origin-url

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
+const gitRemoteOriginUrl = require('git-remote-origin-url');
+
 console.log(getCurrentDir());
+(async () => {
+  try {
+    const remoteUrl = await gitRemoteOriginUrl(process.cwd());
+    console.log(`remote origin url is ${remoteUrl}`);
+  } catch (e) {
+    console.error(e.message);
+  }
+})();
 
 function getCurrentDir(): string {
   const currentDir = process.cwd();
